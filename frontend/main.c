63f12e9f82c947ffdc9c3a3ae7074d8b6be3a6e4
David JR <david@DavidDator.daviddator.net>
2024-03-08 18:27:13 +0100

    fredag

--- frontend/main.c
+++ frontend/main.c
@@ -12,10 +12,15 @@
 
 #include "../core/head/strings.h"
 
+void argsError(const char* msg){
+    fprintf(stderr, "%s\n", msg);
+}
+
+
 int main(int argc, char* argv[]){
 
     // check
-    if(argc > 2){
+    if(argc >= 2){
         if(string_compare(argc[argv-1], "--version") == 0){
             printf("%s\n", "v0.0002");
             exit(0);
@@ -24,8 +29,9 @@ int main(int argc, char* argv[]){
             printf("%s\n", argv[argc-1]);
             FILE* file = readFile((char*)argv[argc-1]); // for clarity
 
+
             if(file == (FILE*)1){
-                fprintf(stderr, "could not open file\n");
+                argsError("failed to open file");
                 return 1;
             }
             // run the interpeter
@@ -35,9 +41,16 @@ int main(int argc, char* argv[]){
 
             return 0;
         }
-    }else{
-        fprintf(stderr, "to few arguments\n");
+        if(string_compare(argc[argv-2], "-e") == 0){
+                printf("%s\n", "eval file");
+        }
+        if(string_compare(argc[argv-1], "-v") == 0){
+                printf("%s\n", "MTPL - Memory unsafe inTerpeted Programing Language, version 1.0");
+        }
     }
+
+
+
     // start parsing the file
 
 
